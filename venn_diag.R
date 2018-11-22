4fcf924257312e462adbe4521a83575c589f961b
bbarres <benoit.barres@gmail.com>
2018-11-22 17:09:20 +0100

    version alpha of the Venn Diagram

--- venn_diag.R
+++ venn_diag.R
@@ -6,18 +6,32 @@
 
 #loading the packages necessary for the analysis
 library(eulerr)
-
+library(RColorBrewer)
 
 
 ###############################################################################
 #loading and preparing the AOX resistance data
 ###############################################################################
 
-fit<-euler(c(A=12,B=20,C=62,"A&B"=3,
-      "A&C"=4,"B&C"=7,"A&B&C"=2))
+#building the contingency table
+div_fin<-table(data_monitoring$participation_financement)
+#total number of evaluated monitoring
+print(paste("Total number of evaluated monitoring =",sum(div_fin)))
+#number of monitoring with no information on the founding sources
+print(paste("Missing data = ",div_fin[1]))
+
+#chosing a set of colors
+colovec<-brewer.pal(3,"Dark2")
+
+fit<-euler(c(Academic=23,Public=36,Private=55,"Academic&Public"=7,
+      "Academic&Private"=5,"Public&Private"=36,"Academic&Public&Private"=12))
 plot(fit,quantities=TRUE)
-plot(fit,quantities=TRUE,col=c("red","green","blue"),lwd=4,cex=4,
-     labels=c("Academic","Private","Public"))
+plot(fit,
+     quantities=list(col=c("black"),cex=1.3,font=3),
+     edges=list(col=colovec,lwd=7),
+     labels=list(col=colovec,cex=1.4),
+     fills=list(fill=colovec,alpha=0.2),
+     legend=FALSE)
 
 
 
