73b4d6ebdf710e657be299d59a5994c1b06e612b
bbarres <benoit.barres@gmail.com>
2018-11-29 23:56:34 +0100

    modifications after homogenization meeting

--- stacked_bar.R
+++ stacked_bar.R
@@ -1,4 +1,5 @@
 ###############################################################################
+###############################################################################
 #Code for the stacked barplot
 ###############################################################################
 ###############################################################################
@@ -31,16 +32,16 @@ effectif
 IDH_coex<-prop.table(IDH_coex,margin=2)*100
 
 
-layout(rbind(1,2),heights=c(8,2))  # put legend on bottom 1/8th of the chart
+layout(cbind(1,2),widths=c(8,3))  # put legend on bottom 1/8th of the chart
 op<-par(mar=c(2,3,2,1))
 temp<-barplot(IDH_coex,col=colovec,border=NA,axes=FALSE,
               axisnames=FALSE,space=0.7,xpd=FALSE)
-axis(1,at=temp,labels=names(effectif),lwd=5,font=2,
+axis(1,at=temp,labels=names(effectif),lwd=4,font=2,
      cex.axis=1.2,padj=0.1,xpd=TRUE,las=1)
-axis(2,lwd=5,font=2,cex.axis=1.5,las=1)
+axis(2,lwd=4,font=2,cex.axis=1.5,las=1)
 #text(temp,par("usr")[1]-10,labels=names(effectif),srt=45,
 #     xpd=TRUE,cex=1.5,font=2)
-box(bty="l",lwd=5)
+box(bty="l",lwd=4)
 text(temp[1],104,paste("n=",effectif[1],sep=""),font=3,cex=1.5,xpd=TRUE)
 text(temp[2],104,paste("n=",effectif[2],sep=""),font=3,cex=1.5,xpd=TRUE)
 text(temp[3],104,paste("n=",effectif[3],sep=""),font=3,cex=1.5,xpd=TRUE)
@@ -50,7 +51,7 @@ text(temp[5],104,paste("n=",effectif[5],sep=""),font=3,cex=1.5,xpd=TRUE)
 par(mar=c(0,0,0,0))
 # c(bottom, left, top, right)
 plot.new()
-legend('center','groups',legend=row.names(IDH_coex),
-       fill=colovec,bty ="n",horiz=FALSE,ncol=2,x.intersp)
+legend('center','groups',legend=row.names(IDH_coex),border="transparent",
+       fill=colovec,bty ="n",horiz=FALSE,ncol=1,cex=1.5)
 par(op)
 
