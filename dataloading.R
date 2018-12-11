a54c5cb88fd89bac00030e169edb96d6e5d54a86
bbarres <benoit.barres@gmail.com>
2018-12-11 16:18:01 +0100

    modification of the pays dataset

--- dataloading.R
+++ dataloading.R
@@ -5,7 +5,7 @@
 ###############################################################################
 
 
-data_pays<-read.csv2(file="data/20181121_data_pays.csv",header=TRUE)
+data_pays<-read.csv2(file="data/20181211_data_pays.csv",header=TRUE)
 
 data_repondants<-read.csv2(file="data/20181114_data_repondant.csv",header=TRUE)
 
