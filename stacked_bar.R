1ae3f20152f844f12f3402b1988a2e1a5745efae
bbarres <benoit.barres@gmail.com>
2018-12-11 18:34:10 +0100

    addition of Figure 5

--- stacked_bar.R
+++ stacked_bar.R
@@ -69,7 +69,7 @@ IDH_PPPsurv<-table(data_pays$q136_q137_UsagePPP,
 #we
 colnames(IDH_PPPsurv)<-c("[0.5-0.6]","[0.6-0.7]","[0.7-0.8]",
                          "[0.8-0.9]","[0.9-1]")
-row.names(IDH_PPPsurv)<-c("Not\nmonitorred","Don't know",
+row.names(IDH_PPPsurv)<-c("Not\nmonitored","Don't know",
                           "Sales","Sales and\nSurveys")
 IDH_PPPsurv<-IDH_PPPsurv[c(3,4,1,2),]
 effectif<-colSums(IDH_PPPsurv)
@@ -101,4 +101,50 @@ legend('left',legend=row.names(IDH_PPPsurv),border="transparent",
        fill=colovec,bty ="n",horiz=FALSE,cex=0.9,y.intersp=2)
 par(op)
 
+
+###############################################################################
+#Figure 5: relation between PPP usage and coexistence of monitoring
+###############################################################################
+
+colovec<-brewer.pal(7,"Dark2")[7:1]
+
+#formating the table necessary for the table
+PPPsurv_coex<-table(data_pays$coexistence_pays,
+                data_pays$q136_q137_UsagePPP)
+#wer remove the NA class
+PPPsurv_coex<-PPPsurv_coex[-c(2),-c(2)]
+colnames(PPPsurv_coex)<-c("Not\nmonitored","Sales","Sales and\nSurveys")
+row.names(PPPsurv_coex)<-c("Academic","Academic\nPrivate",
+                       "Private","Academic\nPublic",
+                       "Public","Academic\nPrivate\nPublic",
+                       "Public\nPrivate")
+PPPsurv_coex<-PPPsurv_coex[c(3,1,5,7,4,2,6),]
+effectif<-colSums(PPPsurv_coex)
+effectif
+PPPsurv_coex<-prop.table(PPPsurv_coex,margin=2)*100
+PPPsurv_coex
+
+layout(cbind(1,2),widths=c(9,2))  # put legend on bottom 1/8th of the chart
+op<-par(mar=c(6.1,5.1,2,0))
+temp<-barplot(PPPsurv_coex,col=colovec,border=NA,axes=FALSE,
+              axisnames=FALSE,space=1.5,xpd=FALSE)
+axis(1,at=temp,labels=FALSE,lwd=4,font=2,
+     cex.axis=1.1,padj=0.1,xpd=TRUE,las=1)
+text(temp,par("usr")[1]-12,labels=names(effectif),srt=0,
+     xpd=TRUE,cex=1.2,font=2)
+axis(2,lwd=4,font=2,cex.axis=1.2,las=1)
+box(bty="l",lwd=4)
+title(main=NULL,xlab="PPP monitoring",ylab="Percentage of countries",cex.lab=2,
+      line=3.5,font.lab=2)
+text(temp[1],104,paste("n=",effectif[1],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[2],104,paste("n=",effectif[2],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[3],104,paste("n=",effectif[3],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[4],104,paste("n=",effectif[4],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[5],104,paste("n=",effectif[5],sep=""),font=3,cex=1.2,xpd=TRUE)
+
+par(mar=c(4.1,0,0,0))
+plot.new()
+legend('topleft',legend=row.names(PPPsurv_coex),border="transparent",
+       fill=colovec,bty ="n",horiz=FALSE,cex=0.9,y.intersp=2)
+par(op)
 
