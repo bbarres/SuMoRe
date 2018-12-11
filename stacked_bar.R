6c22abaa15c1aee64d745d15d84d2c27ce20dedb
bbarres <benoit.barres@gmail.com>
2018-12-11 17:20:45 +0100

    adding figure 4

--- stacked_bar.R
+++ stacked_bar.R
@@ -10,7 +10,7 @@ source("dataloading.R")
 
 
 ###############################################################################
-#loading and preparing the AOX resistance data
+#Figure 3: relation between IDH and organization of reistance monitoring
 ###############################################################################
 
 colovec<-brewer.pal(7,"Dark2")[7:1]
@@ -30,7 +30,7 @@ IDH_coex<-IDH_coex[c(3,1,5,7,4,2,6),]
 effectif<-colSums(IDH_coex)
 effectif
 IDH_coex<-prop.table(IDH_coex,margin=2)*100
-
+IDH_coex
 
 layout(cbind(1,2),widths=c(9,2))  # put legend on bottom 1/8th of the chart
 op<-par(mar=c(6.1,5.1,2,0))
@@ -51,9 +51,54 @@ text(temp[4],104,paste("n=",effectif[4],sep=""),font=3,cex=1.2,xpd=TRUE)
 text(temp[5],104,paste("n=",effectif[5],sep=""),font=3,cex=1.2,xpd=TRUE)
 
 par(mar=c(4.1,0,0,0))
-# c(bottom, left, top, right)
 plot.new()
 legend('topleft',legend=row.names(IDH_coex),border="transparent",
        fill=colovec,bty ="n",horiz=FALSE,cex=0.9,y.intersp=2)
 par(op)
+
+
+###############################################################################
+#Figure 4: relation between IDH and PPP use type of monitoring
+###############################################################################
+
+colovec<-brewer.pal(4,"Accent")[1:4]
+
+#formating the table necessary for the table
+IDH_PPPsurv<-table(data_pays$q136_q137_UsagePPP,
+                   data_pays$Indice.Dvpt.Humain_classes)
+#we
+colnames(IDH_PPPsurv)<-c("[0.5-0.6]","[0.6-0.7]","[0.7-0.8]",
+                         "[0.8-0.9]","[0.9-1]")
+row.names(IDH_PPPsurv)<-c("Not\nmonitorred","Don't know",
+                          "Sales","Sales and\nSurveys")
+IDH_PPPsurv<-IDH_PPPsurv[c(3,4,1,2),]
+effectif<-colSums(IDH_PPPsurv)
+effectif
+IDH_PPPsurv<-prop.table(IDH_PPPsurv,margin=2)*100
+IDH_PPPsurv
+
+layout(cbind(1,2),widths=c(9,2))  # put legend on bottom 1/8th of the chart
+op<-par(mar=c(6.1,5.1,2,0))
+temp<-barplot(IDH_PPPsurv,col=colovec,border=NA,axes=FALSE,
+              axisnames=FALSE,space=0.7,xpd=FALSE)
+axis(1,at=temp,labels=FALSE,lwd=4,font=2,
+     cex.axis=1.1,padj=0.1,xpd=TRUE,las=1)
+text(temp,par("usr")[1]-10,labels=names(effectif),srt=25,
+     xpd=TRUE,cex=1.2,font=2)
+axis(2,lwd=4,font=2,cex.axis=1.2,las=1)
+box(bty="l",lwd=4)
+title(main=NULL,xlab="IDH class",ylab="Percentage of countries",cex.lab=2,
+      line=3.5,font.lab=2)
+text(temp[1],104,paste("n=",effectif[1],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[2],104,paste("n=",effectif[2],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[3],104,paste("n=",effectif[3],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[4],104,paste("n=",effectif[4],sep=""),font=3,cex=1.2,xpd=TRUE)
+text(temp[5],104,paste("n=",effectif[5],sep=""),font=3,cex=1.2,xpd=TRUE)
+
+par(mar=c(4.1,0,0,0))
+plot.new()
+legend('left',legend=row.names(IDH_PPPsurv),border="transparent",
+       fill=colovec,bty ="n",horiz=FALSE,cex=0.9,y.intersp=2)
+par(op)
+
 
