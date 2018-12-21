508f97c4b55f4c8c5bd28b6be64f365b9a8f94cc
bbarres <benoit.barres@gmail.com>
2018-12-21 17:21:34 +0100

    adding the logistic regression model for the PPP usage monitoring type

--- LogRegMonito.R
+++ LogRegMonito.R
@@ -25,8 +25,17 @@ coexreg[coexreg=="NA"]<-0
 coexreg<-as.data.frame(coexreg)
 coexreg<-cbind(coexreg,"IDH"=data_pays$Indice.Dvpt.Humain)
 
+#regression for the academic monitoring
+IDHacad.mod<-glm(Academic~IDH,family="binomial",data=coexreg)
+summary(IDHacad.mod)
+visreg(IDHacad.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Academic",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(Academic monitoring exist)",
+       legend=FALSE,ylim=c(0,1))
+
 #and now the regression, first the public monitoring
 IDHpub.mod<-glm(Public~IDH,family="binomial",data=coexreg)
+summary(IDHpub.mod)
 visreg(IDHpub.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Public",
        overlay=TRUE,partial=FALSE,xlab="IDH",
        ylab="P(Public monitoring exist)",
@@ -34,17 +43,78 @@ visreg(IDHpub.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Public",
 
 #regression for the private monitoring
 IDHpriv.mod<-glm(Private~IDH,family="binomial",data=coexreg)
+summary(IDHpriv.mod)
 visreg(IDHpriv.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Private",
        overlay=TRUE,partial=FALSE,xlab="IDH",
        ylab="P(Private monitoring exist)",
        legend=FALSE,ylim=c(0,1))
 
-#regression for the academic monitoring
-IDHacad.mod<-glm(Academic~IDH,family="binomial",data=coexreg)
+#the 3 plot combined
+op<-par(mfrow=c(1,3))
 visreg(IDHacad.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Academic",
        overlay=TRUE,partial=FALSE,xlab="IDH",
        ylab="P(Academic monitoring exist)",
+       legend=FALSE,ylim=c(0,1),main=list("Academic",cex=2))
+visreg(IDHpub.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Public",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(Public monitoring exist)",
+       legend=FALSE,ylim=c(0,1),main=list("Public",cex=2))
+visreg(IDHpriv.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Private",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(Private monitoring exist)",
+       legend=FALSE,ylim=c(0,1),main=list("Private",cex=2))
+par(op)
+
+#export to a pdf 14 x 5 inches
+
+
+###############################################################################
+#Logistic regression on which tools are used to monitor PPP usage 
+###############################################################################
+
+#formating the data set for the regression
+usaPPP<-as.character(data_pays$q136_q137_UsagePPP)
+usaPPP[usaPPP=="sale"]<-"sale_NA"
+#we make the hypothesis that "no" and "not_know" are equivalent to 
+#"no PPP monitoring at all"
+usaPPP[usaPPP=="no" | usaPPP=="not_known"]<-"NA_NA"
+
+usaPPP<-matrix(unlist(strsplit(usaPPP,"_")),
+               ncol=2,dimnames=list(data_pays$pays,c("Sale","Survey")),
+               byrow=TRUE)
+
+usaPPP[usaPPP %in% c("sale","survey")]<-1
+usaPPP[usaPPP=="NA"]<-0
+usaPPP<-as.data.frame(usaPPP)
+usaPPP<-cbind(usaPPP,"IDH"=data_pays$Indice.Dvpt.Humain)
+
+#and now the regression, first the sale PPP usage monitoring
+IDHsal.mod<-glm(Sale~IDH,family="binomial",data=usaPPP)
+summary(IDHsal.mod)
+visreg(IDHsal.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Sale",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(PPP sale monitoring exist)",
        legend=FALSE,ylim=c(0,1))
+
+#regression for the private monitoring
+IDHsurv.mod<-glm(Survey~IDH,family="binomial",data=usaPPP)
+summary(IDHsurv.mod)
+visreg(IDHsurv.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Survey",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(PPP survey monitoring exist)",
+       legend=FALSE,ylim=c(0,1))
+
+#the 2 plot combined
+op<-par(mfrow=c(1,2))
+visreg(IDHsal.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Sale",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(PPP sale monitoring exist)",
+       legend=FALSE,ylim=c(0,1),main=list("Sale",cex=2))
+visreg(IDHsurv.mod,"IDH",rug=2,scale="response",jitter=TRUE,by="Survey",
+       overlay=TRUE,partial=FALSE,xlab="IDH",
+       ylab="P(PPP survey monitoring exist)",
+       legend=FALSE,ylim=c(0,1),main=list("Survey",cex=2))
+par(op)
 
 
 ###############################################################################
