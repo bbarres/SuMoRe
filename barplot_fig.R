8b3beec6404a6487ab0f33c0fd9d218745d792de
bbarres <benoit.barres@gmail.com>
2019-02-20 01:53:12 +0100

    updating the code for the barplot figures

--- barplot_fig.R
+++ barplot_fig.R
@@ -35,6 +35,8 @@ box(bty="l",lwd=4)
 text(labels=names(VAR),x=graf[1:5],y=rep(-12,5),cex=1.1,font=2,srt=40)
 par(op)
 
+#export to a pdf 6 x 5 inches
+
 
 ###############################################################################
 #Figure 3B: Sampling choice criteria method ~ monitoring device
@@ -72,9 +74,93 @@ axis(2,at=seq(0,70,10),labels=seq(0,70,10),lwd=4,las=1,font=2,cex.axis=1.1)
 box(bty="l",lwd=4)
 mtext(levels(LEVEL)[c(1,3,2)],at=c(1.7,6.1,10.6),line=1.5,cex=1.5,side=1)
 mtext("Type of monitoring system", at=6.1, line=3,cex=1.8,side=1,font=2,padj=1)
-legend(6,80,legend=c("Loss of efficacy" , "Selection pressure" , "Randomly" ),
+legend(6,75,legend=c("Loss of efficacy" , "Selection pressure" , "Randomly" ),
+       pch=15,col=thecol,bg=thecol,bty="n",cex=1.3,pt.cex=1.4,xpd=TRUE,
+       y.intersp=2.5)
+par(op)
+
+#export to a pdf 6 x 5 inches
+
+
+###############################################################################
+#Figure 3E: Frequence de publication ~ monitoring device
+###############################################################################
+
+#picking a "nice" set of colors
+thecol<-brewer.pal(5,"Set1")
+
+#preparing the dataset for the plotting
+VAR<-xtabs(~data_monitoring$q_frequence_publications_res+data_monitoring$type_monitoring_par_repondant)
+VAR<-c(VAR[c(3,1,5,2,4),1],VAR[c(3,1,5,2,4),3],VAR[c(3,1,5,2,4),2])+0.1
+
+#tracer diagramme en bâton
+op<-par(mar=c(6, 5, 1, 1) + 0.1)
+graf<-barplot(VAR,col=rep(thecol,3),ylim=c(0,70),
+              ylab="Number of responses",cex.axis =1.3,cex.lab=1.8,
+              las=1,xaxt="n",yaxt="n",bty="n",border=NA,
+              space=c(rep(0.1,5),1.2,rep(0.1,4),1.2,rep(0.1,4)),
+              font.lab=2)
+
+axis(1,at=graf[c(3,8,13)],labels=FALSE, lwd=4)
+axis(2,at=seq(0,70,10),labels=seq(0,70,10),lwd=4,las=1,font=2,cex.axis=1.1)
+box(bty="l",lwd=4)
+mtext(c("Academic","Governmental","Private"),at=graf[c(3,8,13)],
+      line=1.5,cex=1.5,side=1)
+mtext("Type of monitoring system", at=9.4,line=3,cex=1.8,side=1,
+      font=2,padj=1)
+legend(8,75,legend=c("Not published","During cropping season",
+                       "Yearly","Less frequently","Other"),
+       pch=15,col=thecol,bg=thecol,bty="n",cex=1.3,pt.cex=1.6,xpd=TRUE,
+       ncol=1,x.intersp=1,y.intersp=2.5)
+par(op)
+
+#export to a pdf 6 x 5 inches
+
+
+###############################################################################
+#Figure 4C: Number of respondant by type of monitoring and type of respondant
+###############################################################################
+
+#picking a "nice" set of colors
+thecol<-brewer.pal(11,"PRGn")[c(11,10,9)]
+
+#preparing the dataset for the plotting
+var1<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Academics,data_monitoring$type_monitoring_par_repondant,sum)
+var2<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Officials,data_monitoring$type_monitoring_par_repondant,sum)
+var3<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Companies,data_monitoring$type_monitoring_par_repondant,sum)
+var4<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Advisers,data_monitoring$type_monitoring_par_repondant,sum)
+var5<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Distributors,data_monitoring$type_monitoring_par_repondant,sum)
+var6<-var3+var4+var5
+table<-data.frame(Facteur1=rep(levels(data_monitoring$type_monitoring_par_repondant),3),
+                  Facteur2=c(rep("Academics",3),rep("Officials",3),
+                             rep("Privates",3)),
+                  Count=c(var1,var2,var6))
+#preparing the labels for the plot
+LEVEL<-data_monitoring$type_monitoring_par_repondant
+levels(LEVEL)[3]<-"Governmental"
+
+#plotting the barplot
+op<-par(mar=c(6.5, 5, 1, 1) + 0.1)
+graf<-barplot(table$Count,
+              col=thecol,ylim=c(0,100),ylab="Number of responses",
+              cex.axis =1.3,cex.lab=1.8,las=1,yaxt="n",bty="n",border=NA,
+              space=c(rep(0.1,3),1.2,rep(0.1,2),1.2,rep(0.1,2)),font.lab=2)
+axis(1,at=graf[c(2,5,8)],labels=FALSE, lwd=4)
+axis(2,at=seq(0,100,10),labels=seq(0,100,10),lwd=4,las=1,font=2,cex.axis=1.1)
+box(bty="l",lwd=4)
+mtext(levels(LEVEL)[c(1,3,2)],at=c(1.7,6.1,10.6),line=1.5,cex=1.5,side=1)
+mtext("Type of monitoring system", at=6.1, line=3,cex=1.8,side=1,font=2,
+      padj=1)
+legend(8,100,legend=c("Academics","Officials","Privates"),y.intersp=2.5,
        pch=15,col=thecol,bg=thecol,bty="n",cex=1.4,pt.cex=1.6,xpd=TRUE)
 par(op)
+
+#export to a pdf 6 x 5 inches
+
+
+###############################################################################
+#END
+###############################################################################
 
 
 ###############################################################################
@@ -139,80 +225,5 @@ par(op)
 
 
 ###############################################################################
-#Figure 3E: Frequence de publication ~ monitoring device
-###############################################################################
-
-#choix de la couleur des barres
-thecol<-brewer.pal(5,"Set1")
-
-VAR<-xtabs(~data_monitoring$q_frequence_publications_res+data_monitoring$type_monitoring_par_repondant)
-VAR<-c(VAR[c(3,1,5,2,4),1],VAR[c(3,1,5,2,4),3],VAR[c(3,1,5,2,4),2])+0.1
-
-#tracer diagramme en bâton
-op<-par(mar=c(6, 5, 4, 2) + 0.1)
-graf<-barplot(VAR,
-              col=rep(thecol,3),ylim=c(0,70),ylab="Number of responses",
-              cex.axis =1.3,cex.lab=1.8,las=1,xaxt="n",yaxt="n",bty="n",border=NA,
-              space=c(rep(0.1,5),1.2,rep(0.1,4),1.2,rep(0.1,4)),font.lab=2)
-
-axis(1,at=graf[c(3,8,13)],labels=FALSE, lwd=4)
-axis(2,at=seq(0,70,10),labels=seq(0,70,10),lwd=4,las=1,font=2,cex.axis=1.1)
-box(bty="l",lwd=4)
-
-mtext(c("Academic","Governmental","Private"),at=graf[c(3,8,13)],line=1.5,cex=1.5,side=1)
-mtext("Type of monitoring system", at=9.4, line=3,cex=1.8,side=1,font=2,padj=1)
-legend(0.5,80,legend=c("Not published","During cropping season","Yearly","Less frequently","Other"),
-       pch=15,col=thecol,bg=thecol,bty="n",cex=1.3,pt.cex=1.6,xpd=TRUE,ncol=2)
-par(op)
-
-
-###############################################################################
-#Figure 4C: Number of respondant by type of monitoring and type of respondant
-###############################################################################
-
-#choix de la couleur des barres
-thecol<-c("grey30","grey50","grey70")
-
-#calcul des quantités à partir des variables en binaires
-var1<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Academics,data_monitoring$type_monitoring_par_repondant,sum)
-var2<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Officials,data_monitoring$type_monitoring_par_repondant,sum)
-var3<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Companies,data_monitoring$type_monitoring_par_repondant,sum)
-var4<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Advisers,data_monitoring$type_monitoring_par_repondant,sum)
-var5<-tapply(data_monitoring$q_analyzes_and_interprets_the_data_Distributors,data_monitoring$type_monitoring_par_repondant,sum)
-
-#pool des Privates, Advisers, Distributors
-var6<-var3+var4+var5
-
-#création du jeu de données ordonné pour tracer le barplot
-table<-data.frame(Facteur1=rep(levels(data_monitoring$type_monitoring_par_repondant),3),
-                  Facteur2=c(rep("Academics",3),rep("Officials",3),rep("Privates",3)),
-                  Count=c(var1,var2,var6))
-
-#tri pour mettre academ, public, private
-table<-table[c(1,4,7,3,6,9,2,5,8),]
-
-
-#tracer diagramme en bâton
-
-op<-par(mar=c(6.5, 5, 2, 2) + 0.1)
-graf<-barplot(table$Count,
-              col=thecol,ylim=c(0,100),ylab="Number of responses",
-              cex.axis =1.3,cex.lab=1.8,las=1,yaxt="n",bty="n",border=NA,
-              space=c(rep(0.1,3),1.2,rep(0.1,2),1.2,rep(0.1,2)),font.lab=2)
-axis(1,at=graf[c(2,5,8)],labels=FALSE, lwd=4)
-axis(2,at=seq(0,100,10),labels=seq(0,100,10),lwd=4,las=1,font=2,cex.axis=1.1)
-box(bty="l",lwd=4)
-
-LEVEL<-data_monitoring$type_monitoring_par_repondant
-levels(LEVEL)[3]<-"Governmental"
-
-mtext(levels(LEVEL)[c(1,3,2)],at=c(1.7,6.1,10.6),line=1.5,cex=1.5,side=1)
-mtext("Type of monitoring system", at=6.1, line=3,cex=1.8,side=1,font=2,padj=1)
-legend(6,105,legend=c("Academics" ,"Officials" , "Privates" ),
-       pch=15,col=thecol,bg=thecol,bty="n",cex=1.4,pt.cex=1.6,xpd=TRUE)
-par(op)
-
-
-###############################################################################
-#END
+#END OF SUPPLEMENTARY
 ###############################################################################
