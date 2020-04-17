980147a0fb1903d998b48ff1eb12ed7660d32105
bbarres <benoit.barres@gmail.com>
2020-04-17 22:24:35 +0200

    small modification of the code organization

--- dataloading.R
+++ dataloading.R
@@ -5,11 +5,14 @@
 ##############################################################################/
 
 
-data_pays<-read.csv2(file="data/20181211_data_pays.csv",header=TRUE)
+data_pays<-read.csv2(file="data/20181211_data_pays.csv",
+                     header=TRUE)
 
-data_repondants<-read.csv2(file="data/20181114_data_repondant.csv",header=TRUE)
+data_repondants<-read.csv2(file="data/20181114_data_repondant.csv",
+                           header=TRUE)
 
-data_monitoring<-read.csv2(file="data/20181121_data_monitoring.csv",header=TRUE)
+data_monitoring<-read.csv2(file="data/20181121_data_monitoring.csv",
+                           header=TRUE)
 
 
 ##############################################################################/
